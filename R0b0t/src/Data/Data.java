a5c36d0021533c566da7dca4e049dc8e68f1aabb
msgrobot <markusvat@gmail.com>
2015-11-13 17:58:21 +0100

    Update Data.java

--- R0b0t/src/Data/Data.java
+++ R0b0t/src/Data/Data.java
@@ -2,6 +2,7 @@ package Data;
 
 import java.io.Serializable;
 
+//Netzwerkkommunikation
 public abstract class Data implements Serializable {
 
 	private static final long serialVersionUID = -852363574505049825L;
