4437ef913ca99375ef786957abe3f95e71eaca2f
msgrobot <markusvat@gmail.com>
2015-11-13 18:01:00 +0100

    Update M.java

--- R0b0t/src/Data/M/M.java
+++ R0b0t/src/Data/M/M.java
@@ -4,12 +4,13 @@ import java.io.Serializable;
 
 import Data.Data;
 
+//Geschwingikeits und Lautstärkesteuerung
 public class M extends Data implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
 
-	public float speedl;
-	public float speedr;
+	public float speedl; //Geschwindigkeit links
+	public float speedr; //Geschwindigkeit rechts
 	
 	public M(float speedl, float speedr){
 		this.speedl = speedl;
